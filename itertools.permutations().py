03a113d3b41a60133095147020c988ce8470a69f
Shajith <121110865+shajith42@users.noreply.github.com>
2023-01-12 18:33:59 +0530

    Add files via upload

---
+++ itertools.permutations().py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import permutations
+str, i = input().split()
+p=sorted(list(permutations(str, int(i))))
+for i in p:
+    print(''.join(i))
